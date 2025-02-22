dfdec4cd62fe2b1da38c30b1a054a8e48cc4cfef
xiaomatong1234 <56968563+xiaomatong1234@users.noreply.github.com>
2025-02-22 16:10:09 +0800

    Delete day21 directory

--- day21/prac_stu.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, abort, flash, send_from_directory
-app = Flask(__name__)
-@app.route('/')
-def show_static():
-    return render_template("prac_stu.html")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
